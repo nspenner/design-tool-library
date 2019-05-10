5caa851ba807e3cc337b765c4a3a55d7078224f7
Nathan Minchow <nathan.minchow@outlook.com>
2019-05-09 23:51:34 -0500

    Add some content

--- script.js
+++ script.js
@@ -6,27 +6,98 @@ var navigation = new Vue({
         title: "Accessibility",
         links: [
           {
+            name: "color.review Contrast Checker",
+            URI: "https://color.review/"
+          },
+          {
+            name: "MDN Accessibility Overview",
+            URI: "https://developer.mozilla.org/en-US/docs/Learn/Accessibility"
+          },
+          {
             name: "Web AIM Color Contrast Checker",
             URI: "https://webaim.org/resources/contrastchecker/"
           },
           {
             name: "Web AIM Link Contrast Checker",
             URI: "https://webaim.org/resources/linkcontrastchecker/"
+          }
+        ],
+        showChildren: false
+      },
+      {
+        title: "Art and Illustration",
+        links: [
+          {
+            name: "Lukasz Adam Illustrations",
+            URI: "https://lukaszadam.com/illustrations"
           },
           {
-            name: "MDN Accessibility",
-            URI: "https://developer.mozilla.org/en-US/docs/Learn/Accessibility"
+            name: "Undraw Illustrations",
+            URI: "https://undraw.co/illustrations"
           }
         ],
         showChildren: false
       },
       {
         title: "Color",
-        links: ["qwerty"],
+        links: [
+          {
+            name: "coolors.co Palette Generator",
+            URI: "https://coolors.co/"
+          },
+          {
+            name: "culrs.com Pre-generated Palettes",
+            URI: "https://culrs.com/"
+          }
+        ],
+        showChildren: false
+      },
+      {
+        title: "Icons",
+        links: [
+          {
+            name: "Fontawesome",
+            URI: "https://fontawesome.com/"
+          },
+          {
+            name: "Ionicons",
+            URI: "https://ionicons.com/"
+          }
+        ],
         showChildren: false
       },
       {
         title: "Shadow",
+        links: [
+          {
+            name: "cssmatic Box Shadow Generator",
+            URI: "https://www.cssmatic.com/box-shadow"
+          }
+        ],
+        showChildren: false
+      },
+      {
+        title: "Stock Photos and Images",
+        links: [
+          {
+            name: "Unsplash Free Photos",
+            URI: "https://unsplash.com/"
+          }
+        ],
+        showChildren: false
+      },
+      {
+        title: "Fonts and Typography",
+        links: [
+          {
+            name: "Font Pair – For Pairing Google Fonts",
+            URI: "https://fontpair.co/"
+          },
+          {
+            name: "Google Fonts",
+            URI: "https://fonts.google.com/"
+          }
+        ],
         showChildren: false
       }
     ]
